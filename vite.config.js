ea06445b3fa54c5650e2fe460a83e07a785f866c
AdityaRAJHI <134184167+AdityaRAJHI@users.noreply.github.com>
2024-11-28 16:13:56 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/social-dashboard/',
+  base: process.env.NODE_ENV === 'production' ? '/social-dashboard/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
